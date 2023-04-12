cfc146e7dab535bd23f7b58221f74fdf5380d057
Stefaan De Vylder <stefaan.devylder@gmail.com>
2023-04-12 20:25:31 +0200

    Add built square meters

--- DnaVastgoed/Network/DnaSpottoProperty.cs
+++ DnaVastgoed/Network/DnaSpottoProperty.cs
@@ -58,7 +58,8 @@ namespace DnaVastgoed.Network {
                     },
                     ParcelInfo = new ParcelInfo() {
                         OrientationGarden = GetOrientation(),
-                        AmountOfTotalPlotSquareMeters = !string.IsNullOrWhiteSpace(_prop.LotArea) ? int.Parse(_prop.LotArea.Split(" ")[0]) : null
+                        AmountOfTotalPlotSquareMeters = !string.IsNullOrWhiteSpace(_prop.LotArea) ? int.Parse(_prop.LotArea.Split(" ")[0]) : null,
+                        AmountOfBuiltSquareMeters = !string.IsNullOrWhiteSpace(_prop.LivingArea) ? int.Parse(_prop.LivingArea.Split(" ")[0]) : null
                     }
                 },
                 new SpottoTransaction() {
